b84798564dc31329e6d78de53d96e15dcd130547
NjordJ <nj0rd3841@gmail.com>
2022-10-04 18:05:05 +0500

    Passing data to a fragment with safe args

--- app/src/main/java/com/iruda/navigationexample/ui/WelcomeFragment.kt
+++ app/src/main/java/com/iruda/navigationexample/ui/WelcomeFragment.kt
@@ -6,21 +6,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.iruda.navigationexample.databinding.FragmentLoginBinding
+import androidx.navigation.fragment.navArgs
+import com.iruda.navigationexample.databinding.FragmentWelcomeBinding
 
-class LoginFragment : Fragment() {
-    private var _binding: FragmentLoginBinding? = null
+class WelcomeFragment : Fragment() {
+    private var _binding: FragmentWelcomeBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    private val args: WelcomeFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
         val view = binding.root
         return view
     }
@@ -28,11 +31,11 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonConfirm.setOnClickListener {
-            val userName = binding.editTextLogin.text.toString()
-            val password = binding.editTextPassword.text.toString()
+        binding.textViewUsername.text = args.username
+        binding.textViewPassword.text = args.password
 
-            val action = LoginFragmentDirections.actionLoginFragmentToWelcomeFragment(username = userName, password = password)
+        binding.buttonWelcome.setOnClickListener {
+            val action = WelcomeFragmentDirections.actionWelcomeFragmentToHomeFragment()
             findNavController().navigate(action)
         }
     }
